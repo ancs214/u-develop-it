884d14dd83622de4abbddc625c112868893ed538
ancs214 <ancs214@gmail.com>
2022-04-17 16:07:37 -0400

    add ability to connect to database and run SQL query

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mysql = require('mysql2');
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -8,11 +9,34 @@ app.use(express.json());
 
 
 
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // Your MySQL username,
+      user: 'root',
+      // Your MySQL password
+      password: 'Findn3m01!',
+      database: 'election'
+    },
+    console.log('Connected to the election database.')
+  );
+
+
+
+//db object is using query method to run SQL query and execute the callback with all the resulting rows from candidates table
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+  });
+  
+
+
 
 //Default response for ANY other request (Not Found); must be placed last
 app.use((req, res) => {
     res.status(404).end();
 });
+
 
 
 
