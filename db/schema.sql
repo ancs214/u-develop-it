cfe2abacbb16df1fd6ce3d4b03db653c28078612
ancs214 <ancs214@gmail.com>
2022-04-18 18:43:42 -0400

    added parties table and seeded w values

--- db/schema.sql
+++ db/schema.sql
@@ -4,3 +4,10 @@ CREATE TABLE candidates (
   last_name VARCHAR(30) NOT NULL,
   industry_connected BOOLEAN NOT NULL
 );
+
+-- VARCHAR data type must declare a limit on the length. TEXT, on the other hand, can store much longer strings of varying length.
+CREATE TABLE parties (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(50) NOT NULL,
+  description TEXT
+);
