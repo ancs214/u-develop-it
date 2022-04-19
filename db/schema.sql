fa7b92b3705264896225cc734d05bd88ba2312ee
ancs214 <ancs214@gmail.com>
2022-04-18 21:52:37 -0400

    update candidates table to reference parties

--- db/schema.sql
+++ db/schema.sql
@@ -1,13 +1,22 @@
-CREATE TABLE candidates (
-  id INTEGER AUTO_INCREMENT PRIMARY KEY,
-  first_name VARCHAR(30) NOT NULL,
-  last_name VARCHAR(30) NOT NULL,
-  industry_connected BOOLEAN NOT NULL
-);
+DROP TABLE IF EXISTS candidates;
+DROP TABLE IF EXISTS parties;
 
--- VARCHAR data type must declare a limit on the length. TEXT, on the other hand, can store much longer strings of varying length.
+
+
 CREATE TABLE parties (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(50) NOT NULL,
   description TEXT
 );
+
+
+CREATE TABLE candidates (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  party_id INTEGER,
+  industry_connected BOOLEAN NOT NULL,
+  CONSTRAINT fk_party FOREIGN KEY (party_id) REFERENCES parties(id) ON DELETE SET NULL
+);
+
+
