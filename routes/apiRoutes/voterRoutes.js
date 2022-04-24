94e86859d5608de9029eaaca9937a1a94edea1e6
ancs214 <ancs214@gmail.com>
2022-04-24 18:59:26 -0400

    added voter routes

---
+++ routes/apiRoutes/voterRoutes.js
@@ -0,0 +1,121 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../../db/connection');
+const inputCheck = require('../../utils/inputCheck');
+
+
+//GET all voters alphabetical by last name
+router.get('/voters', (req, res) => {
+    const sql = `SELECT * FROM voters ORDER BY last_name`;
+    //SQL query method to execute the callback w all rows from voters
+    //SYNTAX: .query(query function (err, rows/results, fields)
+    db.query(sql, (err, rows) => {
+        if (err) {
+            //if theres an error, respond with a json object
+            //500 error = server error
+            res.status(500).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: rows
+        });
+    });
+});
+
+// GET single voter
+router.get('/voter/:id', (req, res) => {
+    const sql = `SELECT * FROM voters WHERE id = ?`;
+    const params = [req.params.id];
+
+    db.query(sql, params, (err, row) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: row
+        });
+    });
+});
+
+//POST ROUTE; assuming front will send us user's first name, last name, and email 
+router.post('/voter', ({ body }, res) => {
+    // Data validation
+    const errors = inputCheck(body, 'first_name', 'last_name', 'email');
+    if (errors) {
+        res.status(400).json({ error: errors });
+        return;
+    }
+
+    const sql = `INSERT INTO voters (first_name, last_name, email) VALUES (?,?,?)`;
+    const params = [body.first_name, body.last_name, body.email];
+
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: body
+        });
+    });
+});
+
+
+//PUT ROUTE allows users to update their email address
+router.put('/voter/:id', (req, res) => {
+    // Data validation
+    const errors = inputCheck(req.body, 'email');
+    if (errors) {
+      res.status(400).json({ error: errors });
+      return;
+    }
+  
+    const sql = `UPDATE voters SET email = ? WHERE id = ?`;
+    const params = [req.body.email, req.params.id];
+  
+    db.query(sql, params, (err, result) => {
+      if (err) {
+        res.status(400).json({ error: err.message });
+      } else if (!result.affectedRows) {
+        res.json({
+          message: 'Voter not found'
+        });
+      } else {
+        res.json({
+          message: 'success',
+          data: req.body,
+          changes: result.affectedRows
+        });
+      }
+    });
+  });
+
+
+  //DELETE ROUTE
+  router.delete('/voter/:id', (req, res) => {
+    const sql = `DELETE FROM voters WHERE id = ?`;
+  
+    db.query(sql, req.params.id, (err, result) => {
+      if (err) {
+        res.status(400).json({ error: res.message });
+      } else if (!result.affectedRows) {
+        res.json({
+          message: 'Voter not found'
+        });
+      } else {
+        res.json({
+          message: 'deleted',
+          changes: result.affectedRows,
+          id: req.params.id
+        });
+      }
+    });
+  });
+
+
+
+module.exports = router;
